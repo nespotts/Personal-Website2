a4e7c5ea9538dcdd54432cc7c092efe62dc1298e
Nathan Spotts <nathan_spotts@hotmail.com>
2022-11-02 22:47:19 -0400

    Added dblclick Code Hint to Javascript

---
+++ scripts/home.js
@@ -0,0 +1,18 @@
+/// <reference path="../"
+$(document).ready( function() {
+
+  // $('.pop-img').on("mouseenter", {pop: true, amt: 1.05}, popImg).on("mouseleave", {pop: false, amt: 1.05}, popImg);
+  $('#js_skill').on('dblclick', function() {
+    $('#js_hints').toggle();
+  });
+});
+
+
+function popImg(event) {
+  let width = parseInt($(this).css("width"));
+  let ms = 100;
+  if(event.data.pop)
+    $(this).animate({'width': width*event.data.amt + 'px'}, ms);
+  else
+    $(this).animate({'width': width/event.data.amt + 'px'}, ms);
+}
