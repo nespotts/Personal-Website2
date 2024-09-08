432e7256ef7f63cf2e4f6515d086de0b3c1ef014
Nathan Spotts <nspotts@tronair.com>
2024-09-08 19:58:14 -0400

    check in

--- scripts/projects.js
+++ scripts/projects.js
@@ -9,8 +9,18 @@ function initializeEventHandlers() {
 	$(window).on('resize', (event) => {
 		setIframeWidths();
 	});
+	setupScrollEvents();
 }
 
+function setupScrollEvents() {
+	$('.project_link').each((index, element) => {
+		let element_id = $(element).attr('id');
+		let scroll_id = element_id.replace('_link', '');
+		$(element).on('click', (event) => {
+			scrollToELement($('#' + scroll_id), 100);
+		});
+	})
+}
 
 
 function setIframeWidths() {
@@ -18,3 +28,14 @@ function setIframeWidths() {
 	$('.iframe_container').height(width*3/4);
 	$('.iframe_container iframe').height(width*3/4).css('margin', '0px');
 }
+
+
+function scrollToELement(element, offset) {
+	window.scrollTo({
+		top: element.offset().top - offset,
+		behavior: 'smooth'
+	})
+}
+
+
+
